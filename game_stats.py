cee3d36db6c4540644f0523310a943aceb14de08
gavrih <94778510+gavrih@users.noreply.github.com>
2022-01-18 14:13:14 +0200

    Update game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -7,7 +7,7 @@ class GameStats():
         self.reset_stats()
 
         # Start game in an inactive state.
-        self.game_active = True
+        self.game_active = False
 
         # High score should never be reset.
         self.high_score = 0
@@ -16,4 +16,5 @@ class GameStats():
         """Initialize statistics that can change during the game."""
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0
+    
         self.level = 1
